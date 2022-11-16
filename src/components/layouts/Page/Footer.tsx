df58c8d52f08cb23c3beeaafec5c6d23bd0ea7d7
MaximMukhin <91364561+MaximMukhin@users.noreply.github.com>
2022-11-16 22:03:59 +0300

    Создать базовые layouts Page, Header, Footer (#11)

---
+++ src/components/layouts/Page/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from "react";
+
+interface FooterProps {}
+
+export const Footer: React.FC<PropsWithChildren<FooterProps>> = (props) => {
+  const { children } = props;
+
+  return <div>Footer {children}</div>;
+};
---
+++ src/components/layouts/Page/Header.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from "react";
+
+interface HeaderProps {}
+
+export const Header: React.FC<PropsWithChildren<HeaderProps>> = (props) => {
+  const { children } = props;
+
+  return <div>Header {children}</div>;
+};
